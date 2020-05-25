54efc1cdd25e2f40c7926815a8f286a2b5a532bd
zuigend <zuigend@gmail.com>
2020-05-25 19:50:01 +0900

    Add basic php code.

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>getGlobalAddress</title>
+    </head>
+    <body>
+
+        <?php
+            echo $_SERVER["REMOTE_ADDR"];
+        ?>
+    </body>
+</html>
